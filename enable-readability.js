9768c8fa1f807f85a2387310214b7ff6b4153e1a
Evan Martin <martine@danga.com>
2009-10-31 16:16:56 -0700

    update to browser action

--- enable-readability.js
+++ enable-readability.js
@@ -1,38 +1,23 @@
-// BUG 17410: If a content script doesn't use a DOM event, it can get GC'd.
-function dummy(){
-  console.log(chrome.extension);
-}
-window.addEventListener("load", dummy, false);
-
-
 // These don't make it through to the readability-injected <script> because
 // of isolated worlds.  :~(
 var readStyle='style-novel';
 var readSize='size-medium';
 var readMargin='margin-wide';
 
-function readabilityize() {
-  var _readability_script=document.createElement('SCRIPT');
-  _readability_script.type='text/javascript';
-  _readability_script.src=chrome.extension.getURL('readability/readability.js');
-  document.getElementsByTagName('head')[0].appendChild(_readability_script);
-  var _readability_css=document.createElement('LINK');
-  _readability_css.rel='stylesheet';
-  _readability_css.href=chrome.extension.getURL('readability/readability.css');
-  _readability_css.type='text/css';
-  _readability_css.media='screen';
-  document.getElementsByTagName('head')[0].appendChild(_readability_css);
-  var _readability_print_css=document.createElement('LINK');
-  _readability_print_css.rel='stylesheet';
-  _readability_print_css.href=chrome.extension.getURL('readability/readability-print.css');
-  _readability_print_css.media='print';
-  _readability_print_css.type='text/css';
-  document.getElementsByTagName('head')[0].appendChild(_readability_print_css);
-}
+var _readability_script=document.createElement('SCRIPT');
+_readability_script.type='text/javascript';
+_readability_script.src=chrome.extension.getURL('readability/readability.js');
+document.getElementsByTagName('head')[0].appendChild(_readability_script);
+var _readability_css=document.createElement('LINK');
+_readability_css.rel='stylesheet';
+_readability_css.href=chrome.extension.getURL('readability/readability.css');
+_readability_css.type='text/css';
+_readability_css.media='screen';
+document.getElementsByTagName('head')[0].appendChild(_readability_css);
+var _readability_print_css=document.createElement('LINK');
+_readability_print_css.rel='stylesheet';
+_readability_print_css.href=chrome.extension.getURL('readability/readability-print.css');
+_readability_print_css.media='print';
+_readability_print_css.type='text/css';
+document.getElementsByTagName('head')[0].appendChild(_readability_print_css);
 
-chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-  if (window == top) {
-    readabilityize();
-  }
-});
